4b3d7b93946fb15479d31283f3d2e10703f1c486
J M Cherian <jc925@student.le.ac.uk>
2024-09-04 21:30:21 +0100

    added label, text field and button to fxml

--- src/main/java/org/example/weatherapp/startViewController.java
+++ src/main/java/org/example/weatherapp/startViewController.java
@@ -1,14 +1,32 @@
 package org.example.weatherapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class startViewController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField cityTextField;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void enterCity(ActionEvent event) throws IOException {
+        String city = cityTextField.getText();
+
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
     }
+
 }
