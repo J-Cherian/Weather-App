c780522ccabc7330709ea91631c8aca3f4b3ec47
J M Cherian <jc925@student.le.ac.uk>
2024-09-04 21:54:36 +0100

    city is now displayed in view

--- src/main/java/org/example/weatherapp/displayController.java
+++ src/main/java/org/example/weatherapp/displayController.java
@@ -1,4 +1,33 @@
 package org.example.weatherapp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class displayController {
+    @FXML
+    private Label cityLabel;
+
+    // Method to display the city name
+    public void displayCity(String city) {
+        cityLabel.setText(city);
+    }
+
+    @FXML
+    public void backToMain(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("startView.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
--- src/main/java/org/example/weatherapp/startViewController.java
+++ src/main/java/org/example/weatherapp/startViewController.java
@@ -2,6 +2,7 @@ package org.example.weatherapp;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,7 +21,15 @@ public class startViewController {
     private Parent root;
 
     public void enterCity(ActionEvent event) throws IOException {
+
         String city = cityTextField.getText();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/displayView.fxml"));
+        root = loader.load();
+
+        displayController displayController = loader.getController();
+
+        displayController.displayCity(city);
 
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
